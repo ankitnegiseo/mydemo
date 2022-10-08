82a0b4940e7b917547d284b4ca3e2810dc8f4147
Ankit Negi <amankitnegi@gmail.com>
2022-10-08 15:30:44 +0530

    inital commit

---
+++ build/precache-manifest.b3163346b1294470803ad514fa47d908.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "571bb7e1b54b6d9a5c70c3dad82daa56",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fbc2b5a0cf4cdf35c8ab",
+    "url": "/static/css/2.e62bb353.chunk.css"
+  },
+  {
+    "revision": "b2880ae298f191c14415",
+    "url": "/static/css/main.560573e1.chunk.css"
+  },
+  {
+    "revision": "fbc2b5a0cf4cdf35c8ab",
+    "url": "/static/js/2.18f2cc03.chunk.js"
+  },
+  {
+    "revision": "15254115b2ff4c50e4577bb514107508",
+    "url": "/static/js/2.18f2cc03.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b2880ae298f191c14415",
+    "url": "/static/js/main.133bf045.chunk.js"
+  },
+  {
+    "revision": "71e0677ab2f28215227f",
+    "url": "/static/js/runtime-main.b234ba30.js"
+  },
+  {
+    "revision": "0b4ac1dc75df35e169b70d7719afe4cc",
+    "url": "/static/media/notification.0b4ac1dc.ttf"
+  },
+  {
+    "revision": "5bee74caefdf9d0a834915f6c8eeb259",
+    "url": "/static/media/notification.5bee74ca.svg"
+  },
+  {
+    "revision": "651771e1df95c807c99608188d0a4287",
+    "url": "/static/media/notification.651771e1.woff"
+  },
+  {
+    "revision": "c0d3c94cd6112550c51d7d1ed13b9da1",
+    "url": "/static/media/notification.c0d3c94c.eot"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  }
+]);
